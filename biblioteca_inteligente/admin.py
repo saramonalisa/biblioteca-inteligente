b47a807ee30d2bac9dae808de4fcaba005eda330
Sara <saramonaliscs@gmail.com>
2025-02-10 11:45:17 -0300

    registrando models

--- biblioteca_inteligente/admin.py
+++ biblioteca_inteligente/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Livro, Genero, Classificacao, Editora
 
-# Register your models here.
+admin.site.register(Editora)
+admin.site.register(Livro)
+admin.site.register(Genero)
+admin.site.register(Classificacao)
